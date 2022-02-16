fa8b2273c7979d08cd93a1779626a36d8b5e184a
Javier Clavijo <javierclavijog@gmail.com>
2022-02-16 21:19:06 +0100

    Re-create project from scratch

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TandemCloneBackend
 Back end for clone of Tandem web app written in Django REST Framework. Developed as final project for web development degree.
+
+## Todo List
+- [ ] Write base model, make adjustments as needed
+- [ ] Reflect model adjustments on ER diagram
+  - [ ] User model extends default Django user model, so its attributes and relationships must be added
+  - [ ] "Group" model must be renamed "Channel" or something similar, to avoid confusion with user role groups.
+- [ ] Write serializers
+- [ ] Create Postman collection
+- [ ] Write seeders
--- tandem/tandem/urls.py
+++ tandem/tandem/urls.py
@@ -1,4 +1,9 @@
-"""app URL Configuration
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+from users import views
+
+"""tandem URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,11 +18,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-
-from django.urls import include, path
-from rest_framework import routers
-from auth import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
