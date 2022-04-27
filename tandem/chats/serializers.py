3238f331f330a5a0d7e4a88a726605038283f6ce
Javier Clavijo <javierclavijog@gmail.com>
2022-04-27 19:07:43 +0200

    Delete unneeded serializers and ViewSets
    Delete ChatListView. Delete ChannelChatViewSet, ChannelChatListSerializer, UserChatListSerializer. Delete UserChatListSerializer. Add latest message filter to UserChatSerializer and ChannelSerializer. Delete to_representation serializer methods. Add queryset filtering depending on whether the user is staff to chat viewsets and serializers.

--- tandem/chats/serializers.py
+++ tandem/chats/serializers.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.reverse import reverse
 
 from chats.models import UserChat, UserChatMessage, ChannelChatMessage
-from communities.models import Channel
 
 
 class ChatMessageAuthorSerializer(serializers.HyperlinkedModelSerializer):
@@ -31,37 +29,11 @@ class UserChatMessageSerializer(serializers.ModelSerializer):
 
 
 class UserChatSerializer(serializers.ModelSerializer):
-    def to_representation(self, instance):
-        """Add the name of the user who is not the request's user to the chat's representation. Additionally,
-        add a chat type attribute to allow the application to determine the chat's type. """
-        ret = super(UserChatSerializer, self).to_representation(instance)
-        other_user = instance.users.exclude(pk=self.context['request'].user.pk).get()
-        ret['name'] = other_user.username
-        ret['info_url'] = self.context['request'].build_absolute_uri(
-            reverse('customuser-detail', kwargs={'pk': other_user.id})
-        )
-        ret['chat_type'] = 'user'
-        return ret
-
     users = serializers.HyperlinkedRelatedField(
         view_name='customuser-detail',
         queryset=get_user_model().objects.all(),
         many=True
     )
-    messages = UserChatMessageSerializer(many=True)
-
-    class Meta:
-        model = UserChat
-        fields = [
-            'id',
-            'url',
-            'users',
-            'messages'
-        ]
-
-
-class UserChatListSerializer(UserChatSerializer):
-    """Serializer class for the user chat list view. Displays only the latest message sent to the chat."""
     messages = serializers.SerializerMethodField(method_name='get_messages')
 
     def get_messages(self, instance):
@@ -90,46 +62,4 @@ class ChannelChatMessageSerializer(serializers.HyperlinkedModelSerializer):
             'author',
             'content',
             'timestamp'
-        ]
-
-
-class ChannelChatSerializer(serializers.HyperlinkedModelSerializer):
-    def to_representation(self, instance):
-        """Add a chat type attribute to allow the application to determine the chat's type."""
-        ret = super(ChannelChatSerializer, self).to_representation(instance)
-        ret['chat_type'] = 'channel'
-        return ret
-
-    messages = ChannelChatMessageSerializer(many=True, read_only=True)
-    url = serializers.HyperlinkedIdentityField(view_name='channelchat-detail')
-    info_url = serializers.HyperlinkedIdentityField(view_name='channel-detail')
-
-    class Meta:
-        model = Channel
-        fields = [
-            'id',
-            'url',
-            'info_url',
-            'name',
-            'messages'
-        ]
-
-
-class ChannelChatListSerializer(ChannelChatSerializer):
-    """Serializer class for the channel chat list view. Displays only the latest message sent to the chat."""
-    messages = serializers.SerializerMethodField(method_name='get_messages')
-
-    def get_messages(self, instance):
-        queryset = instance.messages.order_by('-timestamp')[:1]
-        return ChannelChatMessageSerializer(queryset, many=True, read_only=True,
-                                            context={'request': self.context['request']}).data
-
-    class Meta:
-        model = Channel
-        fields = [
-            'id',
-            'url',
-            'info_url',
-            'name',
-            'messages'
         ]
