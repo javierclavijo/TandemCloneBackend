36029ed713152562eff9dc7b108db3dac9902c94
Javier Clavijo <javierclavijog@gmail.com>
2022-04-27 20:21:45 +0200

    Add nested user info to UserChat serializer

--- tandem/chats/serializers.py
+++ tandem/chats/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.utils.field_mapping import get_nested_relation_kwargs
 
 from chats.models import UserChat, UserChatMessage, ChannelChatMessage
 
@@ -29,18 +30,32 @@ class UserChatMessageSerializer(serializers.HyperlinkedModelSerializer):
         ]
 
 
-class UserChatSerializer(serializers.ModelSerializer):
-    users = serializers.HyperlinkedRelatedField(
-        view_name='customuser-detail',
-        queryset=get_user_model().objects.all(),
-        many=True
-    )
+class UserChatSerializer(serializers.HyperlinkedModelSerializer):
     messages = serializers.SerializerMethodField(method_name='get_messages')
 
     def get_messages(self, instance):
         queryset = instance.messages.order_by('-timestamp')[:1]
         return UserChatMessageSerializer(queryset, many=True, read_only=True,
                                          context={'request': self.context['request']}).data
+
+    def build_nested_field(self, field_name, relation_info, nested_depth):
+        """
+        Create nested fields for forward and reverse relationships.
+        Source: https://stackoverflow.com/a/50633184
+        """
+
+        class NestedUserSerializer(serializers.HyperlinkedModelSerializer):
+            class Meta:
+                model = get_user_model()
+                depth = nested_depth - 1
+                fields = ['id', 'url', 'username']
+
+        if field_name == 'users':
+            field_class = NestedUserSerializer
+
+        field_kwargs = get_nested_relation_kwargs(relation_info)
+
+        return field_class, field_kwargs
 
     class Meta:
         model = UserChat
@@ -50,6 +65,7 @@ class UserChatSerializer(serializers.ModelSerializer):
             'users',
             'messages'
         ]
+        depth = 1
 
 
 class ChannelChatMessageSerializer(serializers.HyperlinkedModelSerializer):
