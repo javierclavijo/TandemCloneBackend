9455ffc2ea66e36d0fbd081e4ef6f787fb0d11ae
Javier Clavijo <javierclavijog@gmail.com>
2022-04-30 17:03:49 +0200

    Add image resize and compression on upload
    Add optimize_image receiver function in tandem.signals. Add module import at startup to users and communities modules.

---
+++ tandem/tandem/signals.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from communities.models import Channel
+from users.models import CustomUser
+
+
+@receiver(post_save, sender=Channel)
+@receiver(post_save, sender=CustomUser)
+def optimize_image(sender, instance, **kwargs):
+    """
+    Resizes an image and applies compression to it to lower its size.
+    Sources: https://stackoverflow.com/a/13211834, https://stackoverflow.com/a/70686579
+    """
+    with Image.open(instance.image.path) as image:
+        image.thumbnail((400, 400), Image.LANCZOS)
+        image.save(instance.image.path, optimize=True, quality=85)
