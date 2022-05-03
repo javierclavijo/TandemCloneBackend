1c53c9d36c9081da6550c1fdd69e4e3595cc0086
Javier Clavijo <javierclavijog@gmail.com>
2022-05-03 17:16:38 +0200

    Add back view deleted in last commit

--- tandem/users/views.py
+++ tandem/users/views.py
@@ -4,12 +4,13 @@ from rest_framework import permissions, status, parsers
 from rest_framework import viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from common.models import AvailableLanguage, ProficiencyLevel
 from users.models import UserLanguage
-from users.serializers import UserSerializer, UserLanguageSerializer
+from users.serializers import UserSerializer, UserLanguageSerializer, UserPasswordUpdateSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -62,6 +63,28 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(user_object)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @action(detail=True, methods=['patch'])
+    def set_password(self, request, *args, **kwargs):
+        """Update the user's password. Uses a custom serializer class."""
+        instance = self.get_object()
+        try:
+            password = request.data["password"]
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        # Filter the received request data to avoid setting unwanted data
+        data = {"password": password}
+        serializer = UserPasswordUpdateSerializer(instance, data=data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
 
 class UserLanguageViewSet(viewsets.ModelViewSet):
