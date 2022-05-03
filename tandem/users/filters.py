3450ee85e7ad4f888d044955b11531dacbcff4f9
Javier Clavijo <javierclavijog@gmail.com>
2022-05-03 19:02:58 +0200

    Add native languages filter to UserViewSet

--- tandem/users/filters.py
+++ tandem/users/filters.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django_filters import rest_framework as filters
-from django.contrib.auth import get_user_model
+
+from common.models import ProficiencyLevel, AvailableLanguage
+
+user_model = get_user_model()
 
 
 class UserFilter(filters.FilterSet):
@@ -9,13 +13,23 @@ class UserFilter(filters.FilterSet):
     which contains the search term.
     """
     search = filters.CharFilter(method='get_search')
+    native_language = filters.MultipleChoiceFilter(
+        field_name='languages__level',
+        choices=AvailableLanguage.choices,
+        method='get_native_language'
+    )
 
     def get_search(self, queryset, name, value):
         return queryset.filter(
             Q(username__icontains=value) | Q(description__icontains=value)
         )
 
-    class Meta:
-        model = get_user_model()
+    def get_native_language(self, queryset, name, values):
+        return queryset.filter(
+            languages__language__in=values,
+            languages__level=ProficiencyLevel.NATIVE
+        )
 
-        fields = ('search',)
+    class Meta:
+        model = user_model
+        fields = ('search', 'native_language')
