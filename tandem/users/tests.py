4a6032f3284942670c013dfb625b908c4dfb83c5
Javier Clavijo <javierclavijo.dev@gmail.com>
2022-05-25 19:22:27 +0200

    Delete unused tests, add new test data fixture

--- tandem/users/tests.py
+++ tandem/users/tests.py
@@ -1,12 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse
-from rest_framework.test import APIClient, APITestCase, APIRequestFactory, force_authenticate
+from rest_framework.test import APIClient, APITestCase
 
 from common.models import ProficiencyLevel
 from users.models import UserLanguage
-from users.serializers import UserSerializer
-from users.views import UserViewSet
 
 
 class UserCrudTests(APITestCase):
@@ -14,7 +12,7 @@ class UserCrudTests(APITestCase):
 
     client = APIClient()
     user_model = get_user_model()
-    user = user_model.objects.get(username='admin')
+    user = user_model.objects.get(username='test_user')
 
     # Specify data fixtures to be loaded as initial data
     fixtures = ['test_data.json']
@@ -159,104 +157,3 @@ class UserCrudTests(APITestCase):
         self.assertEqual(user_object.username, data['username'])
         self.assertEqual(user_object.email, data['email'])
         self.assertEqual(user_object.description, data['description'])
-
-    def test_password_change_succeeds_with_appropriate_data(self):
-        """
-        Checks whether password change is successful.
-        """
-        data = {"password": "new_password"}
-        user_id = 2
-        url = reverse("customuser-set-password", kwargs={'pk': user_id})
-        response = self.client.patch(url, data=data, format='json')
-        user_object = self.user_model.objects.get(pk=user_id)
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertTrue(self.client.login(username=user_object.username, password=data['password']))
-
-    def test_password_change_fails_if_password_not_specified(self):
-        """
-        Checks whether password change fails and returns the appropriate error code if no 'password' key was
-        specified in the request.
-        """
-        data = {"test": "new_password"}
-        user_id = 2
-        url = reverse("customuser-set-password", kwargs={'pk': user_id})
-        response = self.client.patch(url, data=data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_set_languages_succeeds_with_appropriate_data(self):
-        """
-        Checks whether language list update is successful. Coverage for this test depends on the user's extant
-        language data and request data --ideally, the test's request should include at least a language the user
-        already has with the same level, one that the user also has but with a different level, and another one that
-        the user doesn't already have, as they are all treated differently in the view's code.
-        """
-        data = {
-            "languages": [
-                {
-                    "language": "ES",
-                    "level": "N"
-                },
-                {
-                    "language": "IT",
-                    "level": "B2"
-                },
-                {
-                    "language": "FR",
-                    "level": "A2"
-                },
-            ]
-        }
-        user_id = 2
-
-        factory = APIRequestFactory()
-        url = reverse("customuser-set-languages", kwargs={"pk": user_id})
-        view = UserViewSet.as_view({'patch': 'set_languages'})
-        request = factory.patch(url, data=data, format="json")
-        force_authenticate(request, user=self.user)
-        response = view(request=request, pk=user_id)
-
-        serializer = UserSerializer(
-            instance=self.user_model.objects.get(pk=user_id),
-            context={'request': request}
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['languages'], serializer.data['languages'])
-
-    def test_set_languages_fails_if_no_language_key(self):
-        """
-        Checks that language list update fails and returns the appropriate error code if no 'languages' key was sent
-        in the request's body.
-        """
-        data = {}
-        user_id = 2
-        url = reverse("customuser-set-languages", kwargs={"pk": user_id})
-        response = self.client.patch(url, data=data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_set_languages_fails_if_language_name_not_specified(self):
-        """
-        Tests that language list update fails and returns the appropriate error code if the format of the 'languages'
-        value is incorrect (has no 'language' key).
-        """
-        data = {"languages": [{
-            "level": "A2"
-        }]}
-        user_id = 2
-        url = reverse("customuser-set-languages", kwargs={"pk": user_id})
-        response = self.client.patch(url, data=data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_set_languages_fails_if_language_level_not_specified(self):
-        """
-        Tests that language list update fails and returns the appropriate error code if the format of the 'languages'
-        value is incorrect (has no 'level' key).
-        """
-        data = {"languages": [{
-            "language": "ES"
-        }]}
-        user_id = 2
-        url = reverse("customuser-set-languages", kwargs={"pk": user_id})
-        response = self.client.patch(url, data=data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
